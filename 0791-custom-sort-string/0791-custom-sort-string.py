c4e872ded04741e0564b24bf793302ced39fad0e
esther <105561884+esthersam07@users.noreply.github.com>
2024-03-12 01:29:31 +0530

    Time: 37 ms (46.45%), Space: 16.5 MB (91.47%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.py
+++ 0791-custom-sort-string/0791-custom-sort-string.py
@@ -1,15 +1,17 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
+        #to get the count of each element in string 's'
         temp={}
         for ele in s:
             temp[ele]=1+temp.get(ele,0)
-
+        
         res=[]
+        #making our result based on the 'order' string
         for ele in order:
             if ele in temp:
                 res.append(ele*temp[ele])
                 temp[ele]=0
-
+        #for those elements that remain in s as not in order
         for x in temp:
             if temp[x]!=0:
                 res.append(x*temp[x])
