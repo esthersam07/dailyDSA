2946a0beff2f07a4409854a78cbd0d72975b3b3e
esther <105561884+esthersam07@users.noreply.github.com>
2024-04-11 23:50:26 +0530

    Time: 79 ms (21.48%), Space: 18.1 MB (36.32%) - LeetHub

---
+++ 0402-remove-k-digits/0402-remove-k-digits.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        #monotonic stack
+        #monotonic increase
+        #take an empty stack
+        #add an element if the element before is less than the one being added
+        #if not, remove previous, decrement value of k, and check with the previous again
+        stack = []
+        for i in num:
+            while(stack!=[] and i<stack[-1] and k>0):
+                del stack[-1]
+                k-=1
+            stack.append(i)
+        stack = stack[:len(stack)-k]
+        res = "".join(stack).lstrip("0")
+        return str(res) if res else "0"  
+        
