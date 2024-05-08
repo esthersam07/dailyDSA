d1f5a75cc3a732aaa4919ce78a913f1ba703999e
esther <105561884+esthersam07@users.noreply.github.com>
2024-05-08 20:11:45 +0530

    Time: 59 ms (85.61%), Space: 18.1 MB (5.59%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.py
+++ 0506-relative-ranks/0506-relative-ranks.py
@@ -1,10 +1,18 @@
 class Solution:
     def findRelativeRanks(self, score: List[int]) -> List[str]:
+        
+        #in a dict store rank and the index of it 
         d = {}
         for i in range(len(score)):
             d[score[i]] = i
+            
+        #sort the dictionary based on keys
         sorted_d_keys = dict(sorted(d.items()))
+        
+        #variable to keep eye of the rank
         rank = len(score)
+        
+        #iterating through the org array to make the changes
         for k,v in sorted_d_keys.items():
             if rank == 3:
                 score[v] = "Bronze Medal"
@@ -16,5 +24,6 @@ class Solution:
                 r = str(rank)
                 score[v] = r
             rank -= 1
+            
         return score
         
