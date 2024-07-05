5c076a7816d78907c8890f24b658f29c09e8bcfd
esther <105561884+esthersam07@users.noreply.github.com>
2024-07-06 00:22:08 +0530

    Time: 392 ms (15.21%), Space: 44.1 MB (97.72%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
@@ -6,34 +6,32 @@
 class Solution:
     def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
         res = []
-        mini = 10**9
-        maxi = 0
         l = head
         c = 1
         temp = head.next
+        
         if l.next==None or temp.next == None:
             return [-1,-1]
+        
         while temp.next!=None:
             r = temp.next
             #local maxima
             if l.val<temp.val and temp.val>r.val:
                 res.append(c)
-                mini = min(mini,c)
-                maxi = max(maxi,c)
+            #local minima
             elif l.val>temp.val and temp.val<r.val:
                 res.append(c)
-                mini = min(mini,c)
-                maxi = max(maxi,c)
             c+=1
             l = temp
             temp = temp.next
+            
         if len(res)<2:
             return [-1,-1]
-        min_distance = float('inf')
-        max_distance = res[-1] - res[0]
+        
+        mini = 10**9
+        maxi = res[-1] - res[0]
+        for i in range(1,len(res)):
+            mini = min(mini,res[i]-res[i-1])
     
-        for i in range(1, len(res)):
-            min_distance = min(min_distance, res[i] - res[i - 1])
-    
-        return [min_distance, max_distance]
+        return [mini, maxi]
         
