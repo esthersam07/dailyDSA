a00f14b7a242e56b4ebd4aebbccf1b29fbe89481
esther <105561884+esthersam07@users.noreply.github.com>
2024-09-01 19:02:17 +0530

    Time: 660 ms (91.81%), Space: 23.7 MB (79.94%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.py
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.py
@@ -3,18 +3,9 @@ class Solution:
         if m*n!=len(original):
             return []
         res = []
-        size = 0
-        i = 0
         for r in range(m):
-            temp = []
-            for c in range(n):
-                if i>=len(original):
-                    return []
-                temp.append(original[i])
-                i+=1
-                size += 1
-            res.append(temp)
-        if size!=m*n:
-            return []
+            s = r*n
+            e = s+n
+            res.append(original[s:e])
         return res
         
