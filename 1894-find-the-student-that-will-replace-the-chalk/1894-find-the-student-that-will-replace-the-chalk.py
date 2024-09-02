a220fa75804852f46c5c71fd759e24161ab08718
esther <105561884+esthersam07@users.noreply.github.com>
2024-09-02 20:35:31 +0530

    Time: 581 ms (41.95%), Space: 29.8 MB (90.90%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.py
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.py
@@ -2,12 +2,7 @@ class Solution:
     def chalkReplacer(self, chalk: List[int], k: int) -> int:
         s = sum(chalk)
         r = k%s
-        i = 0
-        while r>=chalk[i]:
+        for i in range(len(chalk)):
+            if chalk[i]>r:
+                return i
             r -= chalk[i]
-            i += 1
-            #full rounds of the array got removed by k%s. So we won't need complete a round and cpome back again in the front
-            if i>=len(chalk):
-                i = 0
-        return i
-        
