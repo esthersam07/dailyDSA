6770b3f3f9ff39d03ac222c62ef1da6362fa2c5c
esther <105561884+esthersam07@users.noreply.github.com>
2024-09-10 01:51:20 +0530

    Time: 660 ms (66.43%), Space: 55 MB (56.96%) - LeetHub

--- 2326-spiral-matrix-iv/2326-spiral-matrix-iv.py
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.py
@@ -5,51 +5,39 @@
 #         self.next = next
 class Solution:
     def spiralMatrix(self, m: int, n: int, head: Optional[ListNode]) -> List[List[int]]:
-        res = []
-        for r in range(m):
-            row = []
-            for c in range(n):
-                row.append(-1)
-            res.append(row)
+        res = [[-1]*n for _ in range(m)]
         temp = head
         l = 0
         r = n
         t = 0
         b = m
-        while(l<r and t<b):
+        while(temp!=None):
             #move left to right
             for i in range(l,r):
-                if temp==None:
-                    break
-                res[t][i] = temp.val
-                temp = temp.next
+                if temp!=None:
+                    res[t][i] = temp.val
+                    temp = temp.next
             t += 1
             #move top to bottom
             for i in range(t,b):
-                if temp==None:
-                    break
-                res[i][r-1] = temp.val
-                temp = temp.next
+                if temp!=None:
+                    res[i][r-1] = temp.val
+                    temp = temp.next
             r -= 1
             
-            if (t < b) :
-                # Traverse from right to left
-                for i in range(r-1,l-1,-1):
-                    if temp==None:
-                        break
+            # Traverse from right to left
+            for i in range(r-1,l-1,-1):
+                if temp!=None:
                     res[b-1][i] = temp.val
                     temp = temp.next
-                b -= 1
+            b -= 1
 
-            if (l < r) :
-                # Traverse from bottom to top
-                for i in range(b-1,t-1,-1):
-                    if temp==None:
-                        break
+            # Traverse from bottom to top
+            for i in range(b-1,t-1,-1):
+                if temp!=None:
                     res[i][l] = temp.val
                     temp = temp.next
-                l += 1
-
+            l += 1
             
         return res
         
