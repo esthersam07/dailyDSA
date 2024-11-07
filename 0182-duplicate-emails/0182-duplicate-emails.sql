d28ac3c538f85488e6745647579ca7f70428be07
esther <105561884+esthersam07@users.noreply.github.com>
2024-11-07 22:01:52 +0530

    Time: 341 ms (96.71%), Space: 0B (100.00%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,7 +1,5 @@
 # Write your MySQL query statement below
 select email
-from (
-    select email, count(email) as ecount
-    from Person
-    group by email) as temp
-where ecount>1
+from Person
+group by email
+having count(email)>1
