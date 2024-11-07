79404693f6f3367caa7d142ef652535418b77117
esther <105561884+esthersam07@users.noreply.github.com>
2024-11-07 22:27:05 +0530

    Time: 986 ms (33.61%), Space: 0B (100.00%) - LeetHub

--- 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
+++ 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
@@ -1,7 +1,6 @@
 # Write your MySQL query statement below
-select u.name as NAME, sum(amount) as BALANCE
+select u.name as NAME, sum(t.amount) as BALANCE
 from users u
-right join Transactions t
-on u.account=t.account
+natural join Transactions t
 group by t.account
 having BALANCE>10000
